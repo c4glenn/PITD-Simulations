13f278b331a2042c69e526a644c6ae1541adca53
Philip Smith <philip@randomsmiths.com>
2024-09-03 23:06:11 -0400

    serve params

--- pitd_robot_controllers/pitd_robot_controllers/param_server.py
+++ pitd_robot_controllers/pitd_robot_controllers/param_server.py
@@ -1,11 +1,27 @@
 import rclpy
 from rclpy.node import Node
 
+from pitd_interfaces.srv import GetParams
+
 class ParamServer(Node):
     def __init__(self) -> None:
         self.__init__('param_server')
+        self.srv = self.create_service(GetParams, '/settings', self.param_callback)
+        self.attacker_speed = 0.8 * self.defender_speed
+        self.defender_speed = 0.2
+        self.attacker_sensing_radius = 1
+        self.defender_sensing_radius = 1
+        self.target_radius = 0.5
         
-    
+
+    def param_callback(self, request, response):
+        response.attacker_speed = self.attacker_speed
+        response.defender_speed = self.defender_speed
+        response.attacker_sensing_radius = self.attacker_sensing_radius
+        response.defender_sensing_radius = self.defender_sensing_radius
+        response.target_radius = self.target_radius
+        return response
+
 def main(args=None):
     rclpy.init(args=args)
     ps = ParamServer()
